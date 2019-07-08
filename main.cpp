3ee6f6186c5c91e68a88155714fc1be84146efe7
Vladimir Tarelkin <vladimirtarelkintab@gmail.com>
2019-07-08 23:22:12 +0300

    123

--- main.cpp
+++ main.cpp
@@ -8,29 +8,6 @@
 
 using namespace std;
 
-/*liststr massive(const string path){
-    string s =read_lines(path);
-    QString qs = QString::fromStdString(s);
-    QStringList qsl = qs.split(' ', QString::SkipEmptyParts);
-    qsl.sort();
-
-    liststr a;
-    for(int i = 1; i < qsl.size();i++)
-    {
-        int k = 1;
-        string s1 = qsl[i-1].toStdString();
-        while(i<qsl.size() && qsl[i-1] == qsl[i]){
-            k++;
-            i++;
-        }
-
-        a.push_back(s1,to_string(k));
-    }
-    return a;
-}
-*/
-
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
